a2468489467b95e44e3d00f3039e7c3903ec5a9a
aujl <58697398+aujl@users.noreply.github.com>
2025-09-16 20:53:33 +0200

    feat: add realised apy horizons to demo reporting

--- tests/test_performance.py
+++ tests/test_performance.py
@@ -43,6 +43,45 @@ def test_nav_and_yield_trajectories(tmp_path: Path) -> None:
     assert yield_path.is_file()
 
 
+def test_horizon_apys_from_nav_and_yield() -> None:
+    idx = pd.date_range("2024-01-01", periods=5, freq="7D", tz="UTC")
+    nav = pd.DataFrame(
+        {
+            "PoolA": [100.0, 101.0, 104.0, 108.0, 110.0],
+            "PoolB": [100.0, 98.0, 99.0, 100.0, 101.0],
+        },
+        index=idx,
+    )
+    lookbacks = {
+        "last 2 weeks": "14D",
+        "last 2 periods": 2,
+        "last 52 weeks": "364D",
+    }
+
+    apy_nav = performance.horizon_apys(nav, lookbacks=lookbacks, value_type="nav")
+
+    latest = nav.iloc[-1]
+    base_2w = nav.loc[: idx[-1] - pd.Timedelta(days=14)].iloc[-1]
+    years_2w = pd.Timedelta(days=14) / pd.Timedelta(days=365)
+    expected_2w = (latest / base_2w) ** (1.0 / years_2w) - 1.0
+    expected_2w.name = "last 2 weeks"
+
+    base_2p = nav.iloc[-(2 + 1)]
+    periods_per_year = pd.Timedelta(days=365) / (idx[1] - idx[0])
+    years_2p = 2 / periods_per_year
+    expected_2p = (latest / base_2p) ** (1.0 / years_2p) - 1.0
+    expected_2p.name = "last 2 periods"
+
+    pd.testing.assert_series_equal(apy_nav["last 2 weeks"], expected_2w)
+    pd.testing.assert_series_equal(apy_nav["last 2 periods"], expected_2p)
+    assert pd.isna(apy_nav.loc["PoolA", "last 52 weeks"])
+    assert pd.isna(apy_nav.loc["PoolB", "last 52 weeks"])
+
+    yields = nav / nav.iloc[0] - 1.0
+    apy_yield = performance.horizon_apys(yields, lookbacks=lookbacks, value_type="yield")
+    pd.testing.assert_frame_equal(apy_nav, apy_yield)
+
+
 def test_cumulative_return_matches_manual_compounding() -> None:
     dates = pd.date_range("2024-01-01", periods=3, freq="D")
     series = pd.Series([0.1, -0.05, 0.02], index=dates)
